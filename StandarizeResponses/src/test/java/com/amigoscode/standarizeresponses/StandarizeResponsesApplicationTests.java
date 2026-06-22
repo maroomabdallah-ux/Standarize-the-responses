1bfac35c296ea7860f62633cee827e55219b8aa4
maro0m <maroomabdallah@gmail.com>
2026-06-22 11:29:03 +0300

    fix project folder

---
+++ StandarizeResponses/src/test/java/com/amigoscode/standarizeresponses/StandarizeResponsesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigoscode.standarizeresponses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandarizeResponsesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
